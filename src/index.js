11c522d7d389bf22384c61ab2dd51f11800be0fe
MonaliGanganeNeosoft <monali.gangane1999@gmail.com>
2023-03-24 19:23:10 +0530

    added unqiue constraint and started cityrepository

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { City } = require("./models/index");
 
 // require("dotenv").config();
 
 const { PORT } = require("./config/serverConfig");
+// const CityRepository = require("./repository/city-repository");
 
 const setupAndStartServer = async () => {
   const app = express();
@@ -12,9 +14,17 @@ const setupAndStartServer = async () => {
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
+    // console.log(db);
     // console.log(process.env);
+
+    // await City.create({
+    //   name: "New Delhi",
+    // });
+
+    // const repo = new CityRepository();
+    // repo.createCity({ name: "pune" });
   });
 };
 
