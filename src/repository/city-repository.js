7060735f0a8f006cd8d9a3d26c2a8bb074a0ac90
MonaliGanganeNeosoft <monali.gangane1999@gmail.com>
2023-03-30 12:48:11 +0530

    completes city repo

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,22 +1,68 @@
 const { City } = require("../models/index");
 class CityRepository {
+  // async createCity({ name }) {
+  //   try {
+  //     const city = await City.create({ name });
+  //     return city;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
+  // async deleteCity(cityId) {
+  //   try {
+  //     await City.destroy({
+  //       where: {
+  //         id: cityId,
+  //       },
+  //     });
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
+
   async createCity({ name }) {
     try {
-      const city = await City.create({ name });
+      const city = await City.create({
+        name,
+      });
       return city;
     } catch (error) {
-      throw error;
+      console.log("Something went wrong");
+      throw { error };
     }
   }
-  async deleteCity(cityId) {
+  async deleteCity({ cityId }) {
     try {
       await City.destroy({
         where: {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
-      throw error;
+      console.log("Something went wrong");
+      throw { error };
+    }
+  }
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went wrong");
+      throw { error };
+    }
+  }
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong");
+      throw { error };
     }
   }
 }
